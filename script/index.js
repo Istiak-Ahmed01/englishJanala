cf3b7a1b0d5b8b15570e6081a7a5ed0c84aae9d4
Istiak-Ahmed01 <istiakefty1@gmail.com>
2025-09-06 11:07:28 +0600

    done modal almost

--- script/index.js
+++ script/index.js
@@ -4,12 +4,76 @@ const loadLessons = () => {
         .then(json => displayLessons(json.data))
 }
 
+const removeActive = () => {
+    const lessonButtons = document.querySelectorAll(".lesson-btn")
+    // console.log(lessonButtons)
+
+    lessonButtons.forEach(btn => btn.classList.remove("active"))
+}
+
+
 const loadLevelWord = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displayLevelWord(data.data))
+        .then(data => {
+            removeActive()
+            const clickedBtn = document.getElementById(`lesson-btn-${id}`)
+            // console.log(clickedBtn)
+            clickedBtn.classList.add("active")
+
+            displayLevelWord(data.data)
+        })
 }
+
+const  loadWordDetail = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/word/${id}`
+   const res =await fetch(url)
+   const  details =await res.json()
+   displayWordDetails(details.data)
+}
+
+
+//  {
+// "word": "Brisk",
+// "meaning": "চটপটে / দ্রুত",
+// "pronunciation": "ব্রিস্ক",
+// "level": 3,
+// "sentence": "He took a brisk walk in the morning.",
+// "points": 3,
+// "partsOfSpeech": "adjective",
+// "synonyms": [
+// "quick",
+// "energetic"
+// ],
+// "id": 27
+// }
+
+const displayWordDetails = (word) => {
+ console.log(word)
+
+ const detailsBox = document.getElementById("details-container")
+ detailsBox.innerHTML = ` <div>
+                    <h2 class="text-2xl font-bold">${word.word} (<i class="fa-solid fa-microphone-lines"></i> :${word.pronunciation})</h2>
+                </div>
+                <div>
+                    <h2 class=" font-bold">Meaning</h2>
+                    <p>${word.meaning}</p>
+                </div>
+                <div>
+                    <h2 class=" font-bold">Example</h2>
+                    <p>${word.sentence}</p>
+                </div>
+                <div>
+                    <h2 class=" font-bold">Synonym</h2>
+                    <span class="btn">Syn1</span>
+                    <span class="btn">Syn2</span>
+                    <span class="btn">Syn3</span>
+                </div>`
+ document.getElementById("word_modal").showModal()
+}
+
+
 
 // "id": 4,
 // "level": 5,
@@ -22,16 +86,26 @@ const displayLevelWord = (words) => {
     const wordContainer = document.getElementById("word-container")
     wordContainer.innerHTML = ""
 
+    if (words.length == 0) {
+        wordContainer.innerHTML = `<div class="text-center col-span-full rounded-xl  space-y-6 font-bangla">
+            <img class="mx-auto" src="./assets/alert-error.png" alt="">
+            <p class="text-xl font-medium text-gray-400 py-10">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+        </div>
+        `
+        return
+    }
+
     words.forEach(word => {
         const card = document.createElement("div")
         card.innerHTML = `
             <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায় নি ।"}</h2>
             <p class="font-semibold">Meaning /Pronounciation</p>
 
-            <div class="text-2xl font-medium fond-bangla">${word.meaning} / ${word.pronunciation}</div>
+            <div class="text-2xl font-medium fond-bangla">${word.meaning ? word.meaning : "অর্থ পাওয়া যায় নি ।"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায় নি ।"}</div>
             <div class="flex justify-between items-center  ">
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
         </div>
@@ -47,13 +121,14 @@ const displayLessons = (lessons) => {
     for (let lesson of lessons) {
         console.log(lesson)
         const btnDiv = document.createElement("div")
-        btnDiv.innerHTML = `    <button onclick="loadLevelWord(${lesson.level_no
-            })" class="btn btn-outline btn-primary"> <i class="fa-solid fa-book-open"></i> Lesson-${lesson.level_no
-            }</button>
+        btnDiv.innerHTML = `    <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn"> <i class="fa-solid fa-book-open"></i> Lesson-${lesson.level_no}</button>
 `
 
         levelContainer.append(btnDiv)
     }
 }
+
+
+
 
 loadLessons()
